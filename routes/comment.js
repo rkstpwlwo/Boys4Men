67bcf176d4886c82cebfebc12ac768b4df3ddb60
rkstpwlwo <78368140+rkstpwlwo@users.noreply.github.com>
2021-12-21 10:05:38 +0900

    Delete routes directory

--- routes/comment.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const controller = require('../controllers');
-
-router.get('/comment/:postId', controller.comment.get);
-router.post('/comment/:postId', controller.comment.post);
-
-module.exports = router;
--- routes/post.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const controller = require('../controllers/genre');
-
-router.get('/post', controller.post.get);
-router.post('/post', controller.post.post);
-router.delete('/post', controller.post.delete);
-
-module.exports = router;
